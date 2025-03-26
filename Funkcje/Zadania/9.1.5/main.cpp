caa992a17765f454cabe558a96138e7d843917fc
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-03-26 08:46:59 +0100

    elo

---
+++ Funkcje/Zadania/9.1.5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void pitagoras() {
+	cout << "Obliczanie a do kwadratu + b do kwadratu = c do kwadratu\n";
+	
+	
+	
+}
+
+
+
+int main() {
+	
+	
+	
+	
+	return 0;
+}
