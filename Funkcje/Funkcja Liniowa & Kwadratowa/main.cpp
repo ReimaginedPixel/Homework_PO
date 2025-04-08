7136bc691d3d40c01fc8b706dc21dcd96c5df132
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-04-08 11:45:21 +0200

    update

--- Funkcje/Funkcja Liniowa & Kwadratowa/main.cpp
+++ Funkcje/Funkcja Liniowa & Kwadratowa/main.cpp
@@ -11,13 +11,19 @@ double miejsce_zerowe(double a, double b) {
 void obliczenia(){
 
 	srand(time(NULL));
+	double tab_miejsce_zerowe[30] = {};
 	int a = 0, b = 0;
 	for(int i = 0; i <= 30; i++){
 		a = rand()%201 - 100;
-		b = rand()%201 - 100;	
-		cout << "Miejsc zerowe funkcji y = " << a << " x + " << b << " to: " << miejsce_zerowe(a,b) << endl;
+		b = rand()%201 - 100;
+		tab_miejsce_zerowe[i] = miejsce_zerowe(a,b);	
+	}
+	
+	for(int i = 0; i <= 30; i++) {
+	cout << "Miejsc zerowe funkcji y = " << a << " x + " << b << " to: " << miejsce_zerowe[i] << endl;
 	}	
 }
+
 //y = ax + b
 int main() {
 	//cout << "Miejsce Zerowe funkcji y = 5x + 6: " << miejsce_zerowe(5,6) << endl;
