a2bf1fc64dcf0aae781afbd41cdc9ab952b58523
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-04-09 09:08:02 +0200

    fix

--- Funkcje/Funkcje Gra/main.cpp
+++ Funkcje/Funkcje Gra/main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+string ktoSilniejszy(int silaBohatera, int silaPotwora){
+	
+	if(silaBohatera > silaPotwora){
+		return "Bohater";
+	}
+	else if(silaPotwora > silaBohatera){
+		return "Potwór";
+	}
+	else{
+		return "Remis";
+	}	
+	
+}
+
 int obliczPozostaleZycie(int ZyciePotwora, int silaBohatera){
 	
 	int NoweZyciePotwora;
@@ -24,6 +38,7 @@ int main() {
 	int silaBohatera;
 	string typPotwora;
 	int ZyciePotwora;
+	int silaPotwora;
 	
 	cout << "Podaj Imie Bohatera! " << endl << ">> ";
 	cin >> imieBohatera;
@@ -33,6 +48,8 @@ int main() {
 	cin >> typPotwora;	
 	cout << "Podaj Zycie Potwora! " << endl << ">> ";
 	cin >> ZyciePotwora;
+	cout << "Podaj Sile Potwora! " << endl << ">> ";
+	cin >> silaPotwora;
 	
 	cout << imieBohatera << " zaatakowal potwora typu " << typPotwora <<"! Potwor ma teraz " << ZyciePotwora << " punktow zycia." << endl;		
 	
@@ -40,6 +57,17 @@ int main() {
 	
 	cout << "Potworowi Zostalo " << ZyciePotwora << " Punktow Zycia" << endl;
 	
+	string wynik = ktoSilniejszy(silaBohatera, silaPotwora);
+	
+	if(wynik == "Bohater"){
+		cout << "Bohater ma przewagê!" << endl;
+	}
+	else if(wynik == "Potwór"){
+		cout << "Potwor dominuje!" << endl;
+	}
+	else{
+		cout << "Si³y s¹ wyrównane." << endl;
+	}
+	
 	return 0;
 }
-
