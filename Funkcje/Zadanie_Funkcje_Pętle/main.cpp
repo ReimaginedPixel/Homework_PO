8e3a8d0d9b97ea9c29f6a4233681c57b21107723
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-04-23 09:01:24 +0200

    petle

--- Funkcje/Zadanie_Funkcje_Pętle/main.cpp
+++ Funkcje/Zadanie_Funkcje_Pętle/main.cpp
@@ -8,23 +8,59 @@ void petla1(int a) {
 	for(int i = 1;i<=a;i++)
 	{
 		cout << endl;
-		for(int i = 1;i<=a;i++){
+		for(int j = 1;j<=a;j++){
 		cout << "X";	
 		}
 		
 	}
-
-
-
 }
+void petla2(int a) {
+    for (int i = 1; i <= a; i++) {
+    	
+        for (int j = 1; j <= i; j++) { 
+            cout << "X";
+        
+		}
+        
+		
+		cout << endl;
+    }
+}
+
+	
+
+
 
 
 int main() {
 	
-	int a;
-	cout << "Podaj Liczbe Wielkosci petli" << endl;
-	cin >> a;
-	petla1(a);
+	int a, b;
+
+	
+	cout << "Wybiez Rodzaj Petli" << endl;
+	cin >> b;
+	switch(b) {
+		case 1: {  // zadanie 1
+		
+			cout << "Podaj Liczbe Wielkosci petli" << endl;
+			cin >> a;		
+		
+			petla1(a);				
+			break;
+		}
+		
+		
+		
+		case 2: { // zadanie 2
+			cout << "Podaj Liczbe Wielkosci petli" << endl;
+			cin >> a;		
+		
+			petla2(a);		
+			break;
+		}
+		
+				
+	}
 	
 	
 	return 0;
