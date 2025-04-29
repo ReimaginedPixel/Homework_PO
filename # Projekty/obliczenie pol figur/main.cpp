80df1e6cd36608b7726fd205976ce7dca1b7b1b6
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-04-29 11:30:39 +0200

    commit

--- # Projekty/obliczenie pol figur/main.cpp
+++ # Projekty/obliczenie pol figur/main.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 
+void linia() {
+	cout << "\n-+===============================+-\n";
+}
 void menu() {
-	cout << "test";
+	cout << "Wybierz Figure";
+	cout << "\n-+=========- Lista Figur -=========+-";
+	int wybor = 0;
+	cin >> wybor; 
 }
 
 int main() { //obliczenie pol figur
 	menu();
 }
+
+
