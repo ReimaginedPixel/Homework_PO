e1bbd8229e3b5c3908bc48418d3430bdd6497a84
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-04-29 11:36:49 +0200

    menu

--- # Projekty/obliczenie pol figur/main.cpp
+++ # Projekty/obliczenie pol figur/main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <locale.h>
 
 using namespace std;
 
 void linia() {
-	cout << "\n-+===============================+-\n";
+	cout << "\n-+=================================+-\n";
 }
 void menu() {
 	cout << "Wybierz Figure";
 	cout << "\n-+=========- Lista Figur -=========+-";
+	cout << "\n    1. Kwadrat        2.Prostok¹t";
+	cout << "\n    3. Trapez         4.Trójk¹t";
+	cout << "\n    5. Równoleg³obok  6.Szeœciok¹t";
+	linia();
+	
+	cout << "\n Wybierz Figure Do Obliczenia\n -> ";
+	
 	int wybor = 0;
 	cin >> wybor; 
 }
 
 int main() { //obliczenie pol figur
+	setlocale(LC_CTYPE, "Polish");
 	menu();
 }
 
