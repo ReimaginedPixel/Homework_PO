d8439b972c560e514169a3b95dcc8c8c88beaab4
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-04-30 09:07:19 +0200

    bug coplile

---
+++ Podprogramy dzielenie plików/1/dzialania.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "dzialania.h"
+#include "dzialania.cpp"
+
+using namespace std;
+
+void dodawanie(int a, int b) {
+	cout << "Suma Liczb a: " << a << " i b: " << b << " to: " << a + b << endl;
+}
+
+double odejmowanie(int a, int b){
+	return a - b;
+}
--- Podprogramy dzielenie plików/1/main.cpp
+++ Podprogramy dzielenie plików/1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "dzialania.h"
+#include "dzialania.cpp"
 
 using namespace std;
 
@@ -9,11 +10,5 @@ int main() {
 	
 }
 
-void dodawanie(int a, int b) {
-	cout << "Suma Liczb a: " << a << " i b: " << b << " to: " << a + b << endl;
-}
 
-double odejmowanie(int a, int b){
-	return a - b;
-}
 
