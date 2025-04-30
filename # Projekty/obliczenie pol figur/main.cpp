8a0bca65ebc953cfbf1d6289fd770ff4d9cf1ae6
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-04-30 08:44:17 +0200

    update

--- # Projekty/obliczenie pol figur/main.cpp
+++ # Projekty/obliczenie pol figur/main.cpp
@@ -3,10 +3,88 @@
 
 using namespace std;
 
+// linia
 void linia() {
 	cout << "\n-+=================================+-\n";
 }
 
+//figury
+void kwadrat() {
+    float a;
+    cout << "\n |> Podaj bok kwadratu: \n -> ";
+    cin >> a;
+    cout << "Pole: " << a * a << endl;
+    cout << "Obwód: " << 4 * a << endl;
+    linia();
+}
+
+void prostokat() {
+    float a, b;
+    cout << "\n |> Podaj d³ugoœæ: \n -> ";
+    cin >> a;
+    cout << " |> Podaj szerokoœæ: \n -> ";
+    cin >> b;
+    cout << "Pole: " << a * b << endl;
+    cout << "Obwód: " << 2 * (a + b) << endl;
+    linia();
+}
+
+void trapez() {
+    float a, b, h, bok1, bok2;
+    cout << "\n |> Podaj podstawê a: \n -> ";
+    cin >> a;
+    cout << " |> Podaj podstawê b: \n -> ";
+    cin >> b;
+    cout << " |> Podaj wysokoœæ h: \n -> ";
+    cin >> h;
+    cout << " |> Podaj bok 1: \n -> ";
+    cin >> bok1;
+    cout << " |> Podaj bok 2: \n -> ";
+    cin >> bok2;
+    cout << "Pole: " << ((a + b) * h) / 2 << endl;
+    cout << "Obwód: " << a + b + bok1 + bok2 << endl;
+    linia();
+}
+
+void trojkat() {
+    float a, h, b, c;
+    cout << "\n |> Podaj podstawê: \n -> ";
+    cin >> a;
+    cout << " |> Podaj wysokoœæ: \n -> ";
+    cin >> h;
+    cout << " |> Podaj bok 2:\n ->  ";
+    cin >> b;
+    cout << " |> Podaj bok 3: \n -> ";
+    cin >> c;
+    cout << "Pole: " << (a * h) / 2 << endl;
+    cout << "Obwód: " << a + b + c << endl;
+    linia();
+}
+
+void rownoleglobok() {
+    float a, h, b;
+    cout << "\n |> Podaj podstawê: \n -> ";
+    cin >> a;
+    cout << " |> Podaj bok s¹siedni: \n -> ";
+    cin >> b;
+    cout << " |> Podaj wysokoœæ: \n -> ";
+    cin >> h;
+    cout << "Pole: " << a * h << endl;
+    cout << "Obwód: " << 2 * (a + b) << endl;
+    linia();
+}
+
+void szesciokat() {
+    float a;
+    cout << "\n |> Podaj d³ugoœæ boku: \n -> ";
+    cin >> a;
+    cout << "Pole: " << (3 * 1.73205 * a * a) / 2 << endl; // sqrt(3) ? 1.73205
+    cout << "Obwód: " << 6 * a << endl;
+    linia();
+}
+
+
+//menu
 void menu() {
 	int wybor = 0;
 	int wyborwyjscie = 0;
@@ -25,40 +103,79 @@ void menu() {
 	cin >> wybor; 
 	
 
-		cout << "\n |> Wyjsc Z Programu? 1 = tak,  0 = nie \n -> ";
-		cin >> wyborwyjscie;
-		if(wyborwyjscie == 1)
-		{
-			break;
-		}
-		else
-		{
-			wyborwyjscie = 0;
-		}
+
 		
 		
 		
 
-	switch(wybor) {
-		
-		case 1:
-			{
-				
-				break;
-			}
-			
-		default:
-		{
-			cout << " |> Poda³eœ Z³¹ Liczbe!";
-			break;
-		}
-		
+switch(wybor) {
+    case 1: {
+        linia();
+        cout << "Wybrano Kwadrat\n";
+        kwadrat();
+        break;
+    }
+
+    case 2: {
+        linia();
+        cout << "Wybrano Prostok¹t\n";
+        prostokat();
+        break;
+    }
+
+    case 3: {
+        linia();
+        cout << "Wybrano Trapez\n";
+        trapez();
+        break;
+    }
+
+    case 4: {
+        linia();
+        cout << "Wybrano Trójk¹t\n";
+        trojkat();
+        break;
+    }
+
+    case 5: {
+        linia();
+        cout << "Wybrano Równoleg³obok\n";
+        rownoleglobok();
+        break;
+    }
+
+    case 6: {
+        linia();
+        cout << "Wybrano Szeœciok¹t\n";
+        szesciokat();
+        break;
+    }
+
+
+    default: {
+        linia();
+        cout << " |> Poda³eœ z³¹ liczbê!\n";
+        break;
+    }
+
 		
 		
 		
 		
 	} //zamkniecie switch
 	
+		cout << "\n |> Wyjsc Z Programu? 1 = tak,  0 = nie \n -> ";
+		cin >> wyborwyjscie;
+		if(wyborwyjscie == 1)
+			{
+				break;
+			}
+		else
+			{
+				wyborwyjscie = 0;
+			}
+		
+		
 	} //zamkniecie while
 		
 } //zamkniecie funkcji
