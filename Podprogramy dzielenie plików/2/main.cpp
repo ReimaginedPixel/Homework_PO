463a98a3df360cd50e71f242b3af25170a28ef57
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-05-13 11:49:41 +0200

    funkcje

--- Podprogramy dzielenie plików/2/main.cpp
+++ Podprogramy dzielenie plików/2/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-
+/*
 int main()
 {
 	int a=0, b=5;
@@ -18,4 +18,24 @@ void swap( int c,int d)
 	c = d;
 	d = temp;
 	cout<<"W funkcji SWAP zmienne wynosza a=" << c << " teraz b= " << d << "\n";
+}
+*/
+
+void swap(int c, int d);
+
+int main()
+{
+	int a=0,b=5;
+	cout<<"Przed wejœciem do funkcji zmienne wynosz¹ a= "<<a<<" teraz b= "<<b<<endl;
+	swap(a,b);
+	cout<<"teraz wyjsciu z funkcji zmienne wynosza a= "<<a<<" teraz b= "<<b<<endl;
+	system("pause");
+}
+
+void swap(int c, int d)
+{
+	int temp = c;
+	c=d;
+	d=temp;
+	cout<<"w funkcji swap zmienne wynosza a= "<<c<<" teraz b= "<<d<<endl;
 }
