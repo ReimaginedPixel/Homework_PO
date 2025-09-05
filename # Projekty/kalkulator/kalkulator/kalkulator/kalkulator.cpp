ed9676fd2657c851a5ac6998a0ebdd69dba4fb71
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-09-05 10:56:02 +0200

    code fix

--- # Projekty/kalkulator/kalkulator/kalkulator/kalkulator.cpp
+++ # Projekty/kalkulator/kalkulator/kalkulator/kalkulator.cpp
@@ -1,16 +1,19 @@
 ﻿#include <iostream>
+#include <limits>
 
 using namespace std;
 
 void logo()
 {
-    cout << " _        _ _          _       _               \n";
-    cout << "| |       | | |        | |     | |             \n";
-    cout << "| | ____ _| | | ___   _| | __ _| |_ ___  _ __  \n";
-    cout << "| |/ / _` | | |/ / | | | |/ _` | __/ _ \| '__| \n";
-    cout << "|   < (_| | |   <| |_| | | (_| | || (_) | |    \n";
-    cout << "|_|\_\__,_|_|_|\_\\__,_|_|\__,_|\__\___/|_|    \n";
+    cout << R"( _        _ _          _       _               
+| |       | | |        | |     | |             
+| | ____ _| | | ___   _| | __ _| |_ ___  _ __  
+| |/ / _` | | |/ / | | | |/ _` | __/ _ \| '__| 
+|   < (_| | |   <| |_| | | (_| | || (_) | |    
+|_|\_\__,_|_|_|\_\\__,_|_|\__,_|\__\___/|_|    
+)" << endl; //r zapewnia ze sa spacje
 }
+
 void menu()
 {
     cout << "\n========== MENU KALKULATORA ==========\n";
@@ -25,6 +28,63 @@ void menu()
     cout << "Wybierz opcje: ";
 }
 
+// Funkcja wczytuje liczbę zmiennoprzecinkową od użytkownika
+// Parametr: komunikat – tekst wyświetlany przy proszeniu o liczbę
+// Zwraca: wprowadzoną liczbę typu double
+double wczytajLiczbe(const string& komunikat)
+{
+    double liczba;
+    cout << komunikat;
+
+    // Pętla wykonuje się, dopóki użytkownik nie poda poprawnej liczby
+    while (!(cin >> liczba))
+    {
+        cout << "Blad: Wprowadz prawidlowa liczbe!\n";
+        cout << komunikat;
+        cin.clear(); // Resetuje flagę błędu strumienia
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Odrzuca niepoprawne dane z bufora
+    }
+
+    return liczba;
+}
+
+// Funkcja wczytuje liczbę całkowitą od użytkownika
+// Parametr: komunikat – tekst wyświetlany przy proszeniu o liczbę całkowitą
+// Zwraca: wprowadzoną liczbę typu int
+int wczytajCalkowita(const string& komunikat)
+{
+    int liczba;
+    cout << komunikat;
+
+    // Pętla wykonuje się, dopóki użytkownik nie poda poprawnej liczby całkowitej
+    while (!(cin >> liczba))
+    {
+        cout << "Blad: Wprowadz prawidlowa liczbe calkowita!\n";
+        cout << komunikat;
+        cin.clear(); // Resetuje flagę błędu strumienia
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Odrzuca niepoprawne dane z bufora
+    }
+
+    return liczba;
+}
+
+// Funkcja wczytuje wybór opcji od użytkownika
+// Zwraca: numer wybranej opcji (int)
+int wczytajWybor()
+{
+    int wybor;
+
+    // Pętla wykonuje się, dopóki użytkownik nie poda poprawnego numeru opcji
+    while (!(cin >> wybor))
+    {
+        cout << "Blad: Wprowadz prawidlowy numer opcji!\n";
+        cout << "Wybierz opcje: ";
+        cin.clear(); // Resetuje flagę błędu strumienia
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Odrzuca niepoprawne dane z bufora
+    }
+
+    return wybor;
+}
 double dodawanie(double a, double b)
 {
     return a + b;
@@ -97,7 +157,15 @@ bool czyKontynuowac()
     char wybor;
     cout << "\nChcesz wykonac kolejne obliczenie? (t/n): ";
     cin >> wybor;
-    return (wybor == 't' || wybor == 'T');
+
+    if (wybor == 't' || wybor == 'T')
+    {
+        system("cls"); // czyści ekran w Windows
+        logo();        // znowu pokazuje logo
+        return true;
+    }
+
+    return false;
 }
 
 int main()
@@ -114,61 +182,55 @@ int main()
     while (kontynuuj)
     {
         menu();
-        cin >> wybor;
+        wybor = wczytajWybor();
 
         switch (wybor)
         {
         case 1: // Dodawanie
             cout << "Podaj pierwsza liczbe: ";
-            cin >> liczba1;
-            cout << "Podaj druga liczbe: ";
-            cin >> liczba2;
+            liczba1 = wczytajLiczbe("Podaj pierwsza liczbe: ");
+            liczba2 = wczytajLiczbe("Podaj druga liczbe: ");
             wynik = dodawanie(liczba1, liczba2);
             cout << "Wynik: " << liczba1 << " + " << liczba2 << " = " << wynik << "\n";
             break;
 
         case 2: // Odejmowanie
-            cout << "Podaj pierwsza liczbe: ";
-            cin >> liczba1;
-            cout << "Podaj druga liczbe: ";
-            cin >> liczba2;
+            liczba1 = wczytajLiczbe("Podaj pierwsza liczbe: ");
+            liczba2 = wczytajLiczbe("Podaj druga liczbe: ");
             wynik = odejmowanie(liczba1, liczba2);
             cout << "Wynik: " << liczba1 << " - " << liczba2 << " = " << wynik << "\n";
             break;
 
         case 3: // Mnozenie
-            cout << "Podaj pierwsza liczbe: ";
-            cin >> liczba1;
-            cout << "Podaj druga liczbe: ";
-            cin >> liczba2;
+            liczba1 = wczytajLiczbe("Podaj pierwsza liczbe: ");
+            liczba2 = wczytajLiczbe("Podaj druga liczbe: ");
             wynik = mnozenie(liczba1, liczba2);
             cout << "Wynik: " << liczba1 << " * " << liczba2 << " = " << wynik << "\n";
             break;
 
         case 4: // Dzielenie
-            cout << "Podaj pierwsza liczbe: ";
-            cin >> liczba1;
-            cout << "Podaj druga liczbe: ";
-            cin >> liczba2;
-            wynik = dzielenie(liczba1, liczba2);
+            liczba1 = wczytajLiczbe("Podaj pierwsza liczbe: ");
+            liczba2 = wczytajLiczbe("Podaj druga liczbe: ");
             if (liczba2 != 0)
             {
+                wynik = dzielenie(liczba1, liczba2);
                 cout << "Wynik: " << liczba1 << " / " << liczba2 << " = " << wynik << "\n";
+            }
+            else
+            {
+                cout << "Blad: Dzielenie przez zero!\n";
             }
             break;
 
         case 5: // Potega
-            cout << "Podaj podstawe: ";
-            cin >> liczba1;
-            cout << "Podaj wykladnik (liczba calkowita): ";
-            cin >> wykladnik;
+            liczba1 = wczytajLiczbe("Podaj podstawe: ");
+            wykladnik = wczytajCalkowita("Podaj wykladnik (liczba calkowita): ");
             wynik = potega(liczba1, wykladnik);
             cout << "Wynik: " << liczba1 << "^" << wykladnik << " = " << wynik << "\n";
             break;
 
         case 6: // Pierwiastek kwadratowy
-            cout << "Podaj liczbe: ";
-            cin >> liczba1;
+            liczba1 = wczytajLiczbe("Podaj liczbe: ");
             if (liczba1 >= 0)
             {
                 wynik = pierwiastek(liczba1);
@@ -177,7 +239,16 @@ int main()
             break;
 
         case 0: // Wyjscie
-            cout << "Dziekuje za skorzystanie z kalkulatora!\n";
+            cout << "Dziekuje za skorzystanie z kalkulatora!\n\n";
+
+            cout << R"(
+ __                .__                
+|  | ______   ____ |__| ____   ____   
+|  |/ /  _ \ /    \|  |/ __ \_/ ___\  
+|    <  <_> )   |  \  \  ___/\  \___  
+|__|__\____/|___|__/__|\_____>\_____> 
+)" << endl; // dla r
+
             kontynuuj = false;
             break;
 
